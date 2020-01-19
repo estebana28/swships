ba705c4629e8ff39cee161554b1ab20f30e269c1
eesteban.arce <eesteban.arce@gmail.com>
2020-01-19 16:29:26 -0300

    Updates

---
+++ precache-manifest.e1471e643d82e31b13a302ec06a6bcbf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36c5a8ddd81cb71679d19d5efce30cb",
+    "url": "/swships/index.html"
+  },
+  {
+    "revision": "d7dc01297cccd9148ff1",
+    "url": "/swships/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "287e49def4dd854c14e8",
+    "url": "/swships/static/css/main.348d3795.chunk.css"
+  },
+  {
+    "revision": "d7dc01297cccd9148ff1",
+    "url": "/swships/static/js/2.1295b48a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/swships/static/js/2.1295b48a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "287e49def4dd854c14e8",
+    "url": "/swships/static/js/main.03d06553.chunk.js"
+  },
+  {
+    "revision": "fa0f6685a0325e58f03e",
+    "url": "/swships/static/js/runtime-main.70c4d332.js"
+  },
+  {
+    "revision": "9dfcfcae93d269733e57354464f04a7f",
+    "url": "/swships/static/media/Empire.9dfcfcae.png"
+  },
+  {
+    "revision": "4e1c5e3f40bb4e95c996adb01a57fa2c",
+    "url": "/swships/static/media/bcg.4e1c5e3f.jpg"
+  },
+  {
+    "revision": "40f137520bcebd9e1d8beb394fd19af5",
+    "url": "/swships/static/media/default.40f13752.jpg"
+  },
+  {
+    "revision": "9abd66b9d19759703d5cf4868e9a4de9",
+    "url": "/swships/static/media/loading.9abd66b9.gif"
+  },
+  {
+    "revision": "3e5f23da4c0e2d07afd1709e28d68d94",
+    "url": "/swships/static/media/logo.3e5f23da.jpg"
+  },
+  {
+    "revision": "ccf9f897f92e206a09ee1d625682355f",
+    "url": "/swships/static/media/star-destroyer-2.ccf9f897.jpg"
+  },
+  {
+    "revision": "e8ef6caa30cb3631392955c032e6087f",
+    "url": "/swships/static/media/star-destroyer.e8ef6caa.jpg"
+  },
+  {
+    "revision": "76b44a87278a5e4da5f99be2d28204a7",
+    "url": "/swships/static/media/tatooine.76b44a87.jpg"
+  },
+  {
+    "revision": "aa96fb6c351925536abe747fa9c44282",
+    "url": "/swships/static/media/trade-federation-cruiser.aa96fb6c.jpg"
+  }
+]);
