19cf24aa0aaeedba65d1c7f1acaa43bf4163d376
estebana28 <eesteban.arce@gmail.com>
2020-01-29 23:17:24 -0300

    Singleship start styling

--- src/pages/SingleShip.js
+++ src/pages/SingleShip.js
@@ -4,6 +4,47 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
 import {ShipContext} from '../context';
+import styled from 'styled-components';
+
+
+
+const SingleShipWrapper = styled.section`
+    .single-ship-images {
+	display: flex;
+	width: 90vw;
+    }
+    .panel {
+	background-size: auto 100%;
+	background-position: center;
+	background-repeat: no-repeat;
+	border-radius: 50px;
+	color: #fff;
+	flex: 0.5;
+	cursor: pointer;
+	height: 80vh;
+	position: relative;
+	margin: 10px;
+	transition: flex 0.7s cubic-bezier(0.05, 0.6, 0.4, 0.9);
+    }
+    .panel.active {
+	flex: 5;
+    }
+    @media screen and (max-width: 480px) {
+	.container {
+		width: 100vw;
+	}
+	
+	.panel:nth-of-type(4) {
+		display: none;
+	}
+	
+	.panel:nth-of-type(5) {
+		display: none;
+	}
+    }
+`;
+
+
 
 export default class SingleShip extends Component {
     constructor(props){
@@ -16,7 +57,6 @@ export default class SingleShip extends Component {
     }
 
     static contextType = ShipContext;
-
 
 
     render() {
@@ -44,10 +84,10 @@ export default class SingleShip extends Component {
                 </Banner>
             </Hero>
 
-            <section className="single-ship">
+            <SingleShipWrapper className="single-ship">
                 <div className="single-ship-images">
                     {images.map((item,index)=>{
-                        return <img key={index} src={item} alt={name} />
+                        return <img key={index} src={item} alt={name} className="panel"/>
                     })}
                 </div>
                 <div className="single-ship-data">
@@ -73,9 +113,26 @@ export default class SingleShip extends Component {
                         <h6>cargo capacity : {cargo_capacity} units</h6>
                     </article>
                 </div>
-            </section>
+            </SingleShipWrapper>
             </>
         )
     }
 }
 
+
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+        console.log("kakaka");
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
